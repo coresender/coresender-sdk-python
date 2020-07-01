76666eb4187042d35b88c34cb3201591dd86ffca
Marcin Sztolcman <marcin.sztolcman@lemondemon.pl>
2020-07-01 11:57:12 +0200

    v1.0.3

--- coresender/__init__.py
+++ coresender/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["init"]
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 import logging
 import os
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='coresender',
-    version='1.0.2',
+    version='1.0.3',
     description='Coresender API for sending email',
     long_description=readme,
     long_description_content_type='text/markdown',
